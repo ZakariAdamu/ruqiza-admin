b7284d1efdd832df9020e894a582076073fb446b
Zakari Adamu <ojonimizakari@gmail.com>
2024-10-08 08:36:49 +0100

    resolve public route error in middleware.ts file

--- middleware.ts
+++ middleware.ts
@@ -13,7 +13,11 @@
 /* this code will make our authentication routes public. Source: Nextjs.org */
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/api/:path*"]);
+const isPublicRoute = createRouteMatcher([
+	"/api/:path*",
+	"/sign-in(.*)",
+	"/sign-up(.*)",
+]);
 
 export default clerkMiddleware((auth, request) => {
 	if (!isPublicRoute(request)) {
