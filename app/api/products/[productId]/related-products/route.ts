4a7284d35cd70e6ff65a7f3d42b3ab61aecad783
Zakari Adamu <ojonimizakari@gmail.com>
2024-10-15 21:05:24 +0100

    related products api created

---
+++ app/api/products/[productId]/related-products/route.ts
@@ -0,0 +1,43 @@
+import Product from "@/lib/models/Product";
+import { connectToDB } from "@/lib/mongoDB";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+	req: NextRequest,
+	{ params }: { params: { productId: string } }
+) => {
+	try {
+		await connectToDB();
+
+		const product = await Product.findById(params.productId);
+
+		if (!product) {
+			return new NextResponse(
+				JSON.stringify({ message: "Product not found" }),
+				{ status: 404 }
+			);
+		}
+
+		const relatedProducts = await Product.find({
+			// Find products with the same category or collections as the current product
+			$or: [
+				{ category: product.category },
+				{ collections: { $in: product.collections } },
+			],
+			// Exclude the current product from the list of related products
+			_id: { $ne: product._id },
+		});
+
+		if (!relatedProducts) {
+			return new NextResponse(
+				JSON.stringify({ message: "No related products found" }),
+				{ status: 404 }
+			);
+		}
+
+		return NextResponse.json(relatedProducts, { status: 200 });
+	} catch (error) {
+		console.log("[related-products_GET]", error);
+		return new NextResponse("Internal Server Error", { status: 500 });
+	}
+};
