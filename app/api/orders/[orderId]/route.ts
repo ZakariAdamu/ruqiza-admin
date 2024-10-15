b037e284ce08b380312c26bea7557d413dd6922c
Zakari Adamu <ojonimizakari@gmail.com>
2024-10-15 23:15:43 +0100

    update files

--- app/api/orders/[orderId]/route.ts
+++ app/api/orders/[orderId]/route.ts
@@ -4,13 +4,16 @@ import Customer from "@/lib/models/Customer";
 import Order from "@/lib/models/Order";
 import Product from "@/lib/models/Product";
 import { connectToDB } from "@/lib/mongoDB";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async ({ params }: { params: { orderId: string } }) => {
+export const GET = async (req: NextRequest) => {
 	try {
 		await connectToDB();
 
-		const orderDetails = await Order.findById(params.orderId).populate({
+		const url = new URL(req.url);
+		const orderId = url.pathname.split("/").pop();
+
+		const orderDetails = await Order.findById(orderId).populate({
 			path: "products.product",
 			model: Product,
 		});
